17555a64ed54bf9a1c78d5a19df83b50ccac32d0
Ai Zhong <aizhong.ds@gmail.com>
2017-09-12 16:15:24 -0700

    initial commit

--- app.py
+++ app.py
@@ -30,45 +30,43 @@ def graph():
 
         dates = np.array(data['Date'], dtype=np.datetime64)
         y = list(dates)
-
-
         p = figure(title="Stock Price Plot for Last Month {}".format(ticker), x_axis_label='Date', y_axis_label='Price',
                    x_axis_type="datetime")
 
         # only plot if a feature is selected
         if request.form.get('feature1'):
-          feature1 = request.form['feature1']
-          x1 = data[feature1]
-          p.line(y, x1.values, legend="{}".format(feature1), line_width=2, color="green")
+            feature1 = request.form['feature1']
+            x1 = data[feature1]
+            p.line(y, x1.values, legend="{}".format(feature1), line_width=1, color="green")
 
         if request.form.get('feature2'):
-          feature2 = request.form['feature2']
-          x2 = data[feature2]
-          p.line(y, x2.values, legend="{}".format(feature2), line_width=2, color="red")
+            feature2 = request.form['feature2']
+            x2 = data[feature2]
+            p.line(y, x2.values, legend="{}".format(feature2), line_width=1, color="red")
 
         if request.form.get('feature3'):
-          feature3 = request.form['feature3']
-          x3 = data[feature3]
-          p.line(y, x3.values, legend="{}".format(feature3), line_width=2, color="blue")
+            feature3 = request.form['feature3']
+            x3 = data[feature3]
+            p.line(y, x3.values, legend="{}".format(feature3), line_width=1, color="blue")
 
         if request.form.get('feature4'):
-          feature4 = request.form['feature4']
-          x4 = data[feature4]
-          p.line(y, x4.values, legend="{}".format(feature4), line_width=2, color="orange")
+            feature4 = request.form['feature4']
+            x4 = data[feature4]
+            p.line(y, x4.values, legend="{}".format(feature4), line_width=1, color="orange")
 
         script, div = components(p)
 
         # if no field is chosen, simply return an error message
         if not request.form.get('feature1') and not request.form.get('feature2') and \
                 not request.form.get('feature3') and not request.form.get('feature4'):
-          return "Please select at least one value to plot!"
+            return "Please select at least one value to plot!"
         # otherwise return the plot
         return render_template('graph.html', div=div, script=script)
 
     # if no ticker is given, simply return a message
     else:
-      return "Please specify the ticker (e.g., GOOG)"
+        return "Please specify the ticker (e.g., GOOG)"
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
