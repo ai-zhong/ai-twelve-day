d14b3f3a807f3d48dd6c7ba3a7cccb597f75b790
Ai Zhong <aizhong.ds@gmail.com>
2017-09-27 20:18:28 -0700

    initial

--- app.py
+++ app.py
@@ -19,48 +19,48 @@ def graph():
         ticker = request.form['ticker']
 
         # get data from quandl API
-        api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/{}/data.json?api_key=DotxFs2bJksfE6SfssR4'.format(ticker)
+        api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/{}.json?api_key=DotxFs2bJksfE6SfssR4'.format(ticker)
         session = requests.Session()
         session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
         raw_data = session.get(api_url)
         txt = raw_data.json()
-        try:
-            col = txt['column_names']
-            data_raw = txt['data']
-            data = pd.DataFrame(data=data_raw, columns=col)[['Date', 'Close','Adj. Close','Open','Adj. Open']]
-            data = data.sort_values("Date", ascending=False).iloc[0:30,:]
+        # try:
+        col = txt['column_names']
+        data_raw = txt['data']
+        data = pd.DataFrame(data=data_raw, columns=col)[['Date', 'Close', 'Adj. Close', 'Open', 'Adj. Open']]
+        data = data.sort_values("Date", ascending=False).iloc[0:30,:]
 
-            dates = np.array(data['Date'], dtype=np.datetime64)
-            y = list(dates)
-            # y = range(len(dates))
-            p = figure(title="Stock Price Plot for Last Month {}".format(ticker), x_axis_label='Date', y_axis_label='Price',
+        dates = np.array(data['Date'], dtype=np.datetime64)
+        y = list(dates)
+        # y = range(len(dates))
+        p = figure(title="Stock Price Plot for Last Month {}".format(ticker), x_axis_label='Date', y_axis_label='Price',
                    x_axis_type="datetime")
 
-            # only plot if a feature is selected
-            if request.form.get('feature1'):
-                feature1 = request.form['feature1']
-                x1 = data[feature1]
-                p.line(y, x1.values, legend="{}".format(feature1), line_width=1, color="green")
+        # only plot if a feature is selected
+        if request.form.get('feature1'):
+            feature1 = request.form['feature1']
+            x1 = data[feature1]
+            p.line(y, x1.values, legend="{}".format(feature1), line_width=1, color="green")
 
-            if request.form.get('feature2'):
-                feature2 = request.form['feature2']
-                x2 = data[feature2]
-                p.line(y, x2.values, legend="{}".format(feature2), line_width=1, color="red")
+        if request.form.get('feature2'):
+            feature2 = request.form['feature2']
+            x2 = data[feature2]
+            p.line(y, x2.values, legend="{}".format(feature2), line_width=1, color="red")
 
-            if request.form.get('feature3'):
-                feature3 = request.form['feature3']
-                x3 = data[feature3]
-                p.line(y, x3.values, legend="{}".format(feature3), line_width=1, color="blue")
+        if request.form.get('feature3'):
+            feature3 = request.form['feature3']
+            x3 = data[feature3]
+            p.line(y, x3.values, legend="{}".format(feature3), line_width=1, color="blue")
 
-            if request.form.get('feature4'):
-                feature4 = request.form['feature4']
-                x4 = data[feature4]
-                p.line(y, x4.values, legend="{}".format(feature4), line_width=1, color="orange")
+        if request.form.get('feature4'):
+            feature4 = request.form['feature4']
+            x4 = data[feature4]
+            p.line(y, x4.values, legend="{}".format(feature4), line_width=1, color="orange")
 
-            script, div = components(p)
+        script, div = components(p)
 
-        except:
-            return 'input ticker maybe wrong'
+        # except:
+        #     return 'input ticker maybe wrong'
 
         # if no field is chosen, simply return an error message
         if not request.form.get('feature1') and not request.form.get('feature2') and \
